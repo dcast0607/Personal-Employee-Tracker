9c99a5425636f2af1a96460b1d78f15af4f6fdad
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 01:37:35 -0700

    Added comments to connection.js file

--- utils/connection.js
+++ utils/connection.js
@@ -1,6 +1,10 @@
+// This file is used to initialize our database connection. We import a few of the
+// required packaged to be able to use our SQL database and then we initialize the
+// connection. This will help us make queries to our database. 
+
 const mysql = require('mysql2');
 const util = require('util');
-require('dotenv').config();
+//require('dotenv').config();
 
 
 const db = mysql.createConnection({
