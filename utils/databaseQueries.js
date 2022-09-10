2f33268ef45b5173f00b558f48dfe29c48667e93
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 15:22:15 -0700

    Pushing comments from userPrompts.js file

--- utils/databaseQueries.js
+++ utils/databaseQueries.js
@@ -1,95 +1,100 @@
 // We initialize "db" so that we can use it on our query requests.
-const db = require('./connection');
- 
+const db = require("./connection");
+
 // We query our database and request to get all data that exists in
-// the department table. 
+// the department table.
 const fetchAllDepartments = () => {
-    return db.query(`SELECT * FROM department`);
+  return db.query(`SELECT * FROM department`);
 };
 
 // We query our database and request to get all data that exists
 // in the role table.
 const fetchAllRoles = () => {
-    return db.query(`SELECT * FROM role`);
+  return db.query(`SELECT * FROM role`);
 };
 
 // We query our database and request to get all data that exists
 // in the employee table.
 const fetchAllEmployees = () => {
-    return db.query(`SELECT * FROM employee`);
+  return db.query(`SELECT * FROM employee`);
 };
 
 // This function is used to insert data into the department table
 // in this particular case we are sending in a department name
 // which will create a new entry in the department table.
 const addDepartment = (name) => {
-    // console.log(name);
-    return db.query(`INSERT INTO department (name) VALUES ("${name}");`);
-
+  // console.log(name);
+  return db.query(`INSERT INTO department (name) VALUES ("${name}");`);
 };
 
 // Similar to the request above, we are inserting data into the role table
 // in this case, however, we are passing in adding parameters and using those
-// values to generate a new role entry in the role table. 
+// values to generate a new role entry in the role table.
 const addRole = (title, salary, department_id) => {
-    return db.query(`INSERT INTO role (title, salary, department_id) VALUES ("${title}", ${salary}, ${department_id});`);
+  return db.query(
+    `INSERT INTO role (title, salary, department_id) VALUES ("${title}", ${salary}, ${department_id});`
+  );
 };
 
 // Very much like the request above, we are passing in data and using this data
-// to create a new table entry in the employee table. 
+// to create a new table entry in the employee table.
 const addEmployee = (first_name, last_name, role_id, manager_id) => {
-    return db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${first_name}", "${last_name}", ${role_id}, ${manager_id});`);
-;}
+  return db.query(
+    `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${first_name}", "${last_name}", ${role_id}, ${manager_id});`
+  );
+};
 
 // This function allows us to update an existing employee role record, we are
 // passing in an UPDATE request, identifying the employee we want to update
 // using it's employee ID and updating this users role by the new ID passed into
 //  this function.
 const updateExistingEmployeeRole = (employee_id, role_id) => {
-    return db.query(
-        `
+  return db.query(
+    `
         UPDATE employee SET role_id = ${role_id} WHERE employee.id = ${employee_id}
-        `);
+        `
+  );
 };
 
 // Very much like the function above, all we are doing is looking up an existing
-// entry on the employee table and then updating this entry with the new 
-// manager_id that was passed along to this function by the user. 
+// entry on the employee table and then updating this entry with the new
+// manager_id that was passed along to this function by the user.
 const updateExistingEmployeeManager = (employee_id, manager_id) => {
-    return db.query(
-        `
+  return db.query(
+    `
         UPDATE employee SET manager_id = ${manager_id} WHERE employee.id = ${employee_id}
-        `);
+        `
+  );
 };
 
 // This particular function allows us to retrieve database entries in the employees
-// table by filtering by a specific manager_id. 
+// table by filtering by a specific manager_id.
 const fetchAllEmployeesByManager = (manager_id) => {
-    return db.query(
-        `
+  return db.query(
+    `
         SELECT * FROM employee WHERE employee.manager_id = ${manager_id}
         `
-    )
+  );
 };
 
-// This function is one of the more challenging functions to work with, 
+// This function is one of the more challenging functions to work with,
 // unlike the other functions this is not a singular database request as
 // some of these entries exist across multiple tables. Given that this
 // is the case, we needed to join a few tables to get the right data that we need.
 
 // You can see that we declare the data that we want on like 84 as well as it's names
 // and we request this data from the employee table by joining additional tables and
-// filtering the data we need by the department_id. 
+// filtering the data we need by the department_id.
 const fetchAllEmployeesByDepartment = (department_id) => {
-    return db.query(
-        `
+  return db.query(
+    `
         SELECT employee.id AS employeeID, employee.first_name, employee.last_name, department.id AS departmentID, department.name AS department
         FROM employee
         LEFT JOIN role ON role_id = role.id
         LEFT JOIN department ON department_id = department.id
         WHERE department.id = ${department_id};
         `
-    )
+  );
 };
 
 // This database query identifies the table for which you want to delete the record
@@ -97,55 +102,54 @@ const fetchAllEmployeesByDepartment = (department_id) => {
 // user from the employee table the request would look something like:
 // DELETE FROM employee WHERE employee.id = someID;
 const deleteDatabaseEntry = (entity_name, entry_id) => {
-    return db.query(
-        `
+  return db.query(
+    `
         DELETE FROM ${entity_name} WHERE ${entity_name}.id = ${entry_id} 
         `
-    )
+  );
 };
 
 // I was able to recycle much of the SQL query from the fetchEmployeesByDepartment function
-// I cleaned it up a bit by removing a few unnecessary columns. We can use the table that 
-// is returned to calculate the budget of said department. 
+// I cleaned it up a bit by removing a few unnecessary columns. We can use the table that
+// is returned to calculate the budget of said department.
 const fetchDepartmentBudget = (department_id) => {
-    return db.query(
-        `
+  return db.query(
+    `
         SELECT employee.id AS employeeID, employee.first_name, employee.last_name, department.id AS departmentID, department.name AS department, role.salary AS salary
         FROM employee
         LEFT JOIN role ON role_id = role.id
         LEFT JOIN department ON department_id = department.id
         WHERE department.id = ${department_id}; 
         `
-    )
+  );
 };
 
 // A few of the inquirer queries that I am using require that we present the user with
 // some information so I created a function that could fetch that data for us. This just
-// sends back data in which the employee and role tables have been join by the role_id. 
+// sends back data in which the employee and role tables have been join by the role_id.
 const fetchEmployeeAndRolesTables = () => {
-    return db.query(`
+  return db.query(`
     SELECT employee.id AS employeeID, employee.first_name, employee.last_name, employee.manager_id, role.title AS role, role.salary, role.id AS role_id, role.department_id
     FROM employee
     LEFT JOIN role ON role_id = role.id
     `);
 };
 
-
 // Exporting my functions here so that I can use them on the inquirer prompts that I will be presenting
-// to the user. 
+// to the user.
 
 module.exports = {
-    fetchAllDepartments,
-    fetchAllRoles,
-    fetchAllEmployees, 
-    addDepartment,
-    addRole,
-    addEmployee,
-    updateExistingEmployeeRole,
-    updateExistingEmployeeManager,
-    fetchAllEmployeesByManager,
-    fetchAllEmployeesByDepartment,
-    deleteDatabaseEntry,
-    fetchDepartmentBudget,
-    fetchEmployeeAndRolesTables
+  fetchAllDepartments,
+  fetchAllRoles,
+  fetchAllEmployees,
+  addDepartment,
+  addRole,
+  addEmployee,
+  updateExistingEmployeeRole,
+  updateExistingEmployeeManager,
+  fetchAllEmployeesByManager,
+  fetchAllEmployeesByDepartment,
+  deleteDatabaseEntry,
+  fetchDepartmentBudget,
+  fetchEmployeeAndRolesTables,
 };
--- utils/userPrompts.js
+++ utils/userPrompts.js
@@ -1,14 +1,27 @@
-const { QuickSight } = require('aws-sdk');
+// We start this file off by bringing in the files and packages that we will be using. 
+// "databaseQueries" is the object that has the functions that we need to utilize
+// throughout this js file. 
 const inquirer = require('inquirer');
 const databaseQueries = require('./databaseQueries');
 
-
+// This function will be used to deliver the prompts and make the database queries
+// requests. 
 const userPrompts = () => {
 
+    // The first function we introduce here is the quit function. This will
+    // allow the user to exit the application if the don't want to continue
+    // interacting with our app. We also send the user to the main menu after
+    // this function finishes, in case they want to keep messing around with
+    // the application.
     const quit= () => {
         console.log("Thank you for use the application!");
+        console.log("Type in CTRL + C and then click enter");
     };
 
+    // This function is used to make a database request that fetches all of the
+    // departments that exist on the department table. Once we get back the data
+    // that we have requested we take that data and use "console.table" to present
+    // this data in a neatly organized table.
     const fetchDepartments = async() => {
        const departmentsData = await databaseQueries.fetchAllDepartments()
        .then((data) => {
@@ -21,6 +34,12 @@ const userPrompts = () => {
        navMenu();
     };
 
+
+    // This function is used to make a database request that fetches all of the
+    // role data that exists in the role table. Once we have the information
+    // we have requested, we present this data to the user using "console.table". 
+    // We also send the user back to the navMenu in case they want to keep
+    // interacting with this app. 
     const fetchRoles = async () => {
         const rolesData = await databaseQueries.fetchAllRoles()
         .then((data) => {
@@ -33,6 +52,9 @@ const userPrompts = () => {
         navMenu();
     };
 
+    // This function works in the same way as the previous two functions, the only
+    // difference is that this time we are retrieving employee information and
+    // displaying that to the end user. 
     const fetchEmployees = async() => {
         const employeesData = await databaseQueries.fetchAllEmployees()
         .then((data) => {
@@ -45,6 +67,12 @@ const userPrompts = () => {
         navMenu();
     };
 
+
+    // createDepartment is called by the createDepartmentPrompts function and a departmentName
+    // parameter is passed so that we can use this name to create a new department entry in our
+    // department table. Once this entry have been created we make a call to the fetchDepartments
+    // function so that we can display a new table that includes the newly entered information
+    // this also takes care of sending the user back to the main menu. 
     const createDepartment = async (departmentName) => {
         if (departmentName) {
             const departmentData = await databaseQueries.addDepartment(departmentName)
@@ -56,12 +84,19 @@ const userPrompts = () => {
                 console.log(err);
             });
         } else {
+
+            // One cool little feature that I added is verification that the user has entered some
+            // department name. If they haven't they will see an error message in red and be taken
+            // back to the createDepartmentPrompts function to try to enter the data again.
             console.log('\x1b[31m%s\x1b[0m', "Whoops, please make sure that you add a department name!");
             createDepartmentPrompts();
         }
     };
 
-
+    // This function works alongside the "createDepartment" function, we are using two functions here
+    // because we first want to collect the name of the department that the user wants to add and then
+    // once we have collected that information we send that to our "createDepartment" function to actually
+    // create the new department entry in our table. 
     const createDepartmentPrompts = async() => {
         inquirer
         .prompt(
@@ -81,6 +116,10 @@ const userPrompts = () => {
         });
     };
 
+    // We collect the role data that the user has entered and validate that they added some data for all
+    // of these fields, in our schema, all of these fields  must be present which is why I am adding this
+    // validation step. If we have collected all the necessary data we create a new entry in the role table
+    // and then log a table so that the user can see that the data was added correctly. 
     const createRole = async(title, salary, department_id) => {
 
         if (title && salary && department_id) {
@@ -99,6 +138,9 @@ const userPrompts = () => {
 
     };
 
+    // The createRolePrompts function works alongside the createRole function, this function is used
+    // to collect data and then pass that data to the createRole function to add the new role to our
+    // database.
     const createRolePrompts = async() => {
 
         const createRoleData = await databaseQueries.fetchAllDepartments()
@@ -138,6 +180,11 @@ const userPrompts = () => {
         });
     };
 
+    // The logic here is similar to the previous functions, the only difference here is that
+    // when it comes to employee data, it is possible to have "null" data entries. We are using
+    // some if statements to check the data so that if there is no data, we can pass null as a value.
+    // Once the user has been created, we generate a new table that should contain the new employee
+    // data. 
     const createEmployee = async(first_name, last_name, role_id, manager_id) => {
         if (first_name && last_name) {
             console.log(first_name + ' ' + last_name);
@@ -168,6 +215,10 @@ const userPrompts = () => {
         }
     }; 
 
+
+    // Similar to the two previous functions, we have a data collection step and a data entry step. 
+    // This function collects the user data and sends it over to our createEmployee function for
+    // validation and data entry.
    const createEmployeePrompts = async() => {
 
     const createEmployeePromptData = await databaseQueries.fetchEmployeeAndRolesTables()
@@ -212,6 +263,8 @@ const userPrompts = () => {
     });
     };  
 
+    // Data is validated and then used to update the existing user record. Employee ID is used
+    // in our "WHERE" clause in our SQL statement and "role ID" is the data that is being updated. 
     const updateExistingEmployeeRole = async(employee_id, role_id) => {
         if (employee_id && role_id) {
             const updateRole = await databaseQueries.updateExistingEmployeeRole(employee_id, role_id)
@@ -230,6 +283,9 @@ const userPrompts = () => {
         };
     };
 
+    // We use this function to display a data table to the user so that they have some reference for 
+    // the data that we are requesting, in this case an employee ID, and then once we collect that data
+    // we ask for a role ID that they want to use to update the employee record. 
     const updateExistingEmployeeRolePrompts = async () => {
         const userData = await databaseQueries.fetchEmployeeAndRolesTables()
         .then((data) => {
@@ -263,6 +319,7 @@ const userPrompts = () => {
         });
     };
 
+    // This function consumes the manager ID and employee ID to update the existing user record.
     const updateManagerEmployeeRecord = async(employee_id, manager_id) => {
         if (employee_id && manager_id) {
             const updateManagerData = await databaseQueries.updateExistingEmployeeManager(employee_id, manager_id)
@@ -280,6 +337,7 @@ const userPrompts = () => {
         };
     };
 
+    // Collects the employee ID and manager ID and passes it to the updateManagerEmployeeRecord function.
     const updateExistingEmployeeManagersPrompts = async() => {
         const employeeData = await databaseQueries.fetchEmployeeAndRolesTables()
         .then((data) => {
@@ -312,6 +370,8 @@ const userPrompts = () => {
         });
     };
 
+    // Takes the manager ID and fetches the employees that exist with this 
+    // manager ID. 
     const fetchEmployeesByManager = async(manager_id) => {
         if (manager_id) {
             const employeeData = await databaseQueries.fetchAllEmployeesByManager(manager_id)
@@ -328,6 +388,7 @@ const userPrompts = () => {
         }
     };
 
+    // Retrieves the manager ID and passes it to the fetchEmployeesByManager function.
     const fetchEmployeesByManagerPrompts = async() => {
         const employeeData = await databaseQueries.fetchAllEmployees()
         .then((data) => {
@@ -356,6 +417,8 @@ const userPrompts = () => {
         });
     }; 
 
+    // Takes the department ID, validates that it is not null/undefined and 
+    // retrieves the requested data.
     const fetchEmployeesByDepartment = async(department_id) => {
         if (department_id) {
             const employeeData = await databaseQueries.fetchAllEmployeesByDepartment(department_id)
@@ -372,6 +435,7 @@ const userPrompts = () => {
         };
     };
 
+    // Collects the department ID and passes it to our fetchEmployeesByDepartment function.
     const fetchEmployeesByDepartmentPrompts = async() => {
         const employeeData = await databaseQueries.fetchAllDepartments()
         .then((data) => {
@@ -400,6 +464,11 @@ const userPrompts = () => {
         });
     };
 
+    // This function validates the data that needs to be removed by filtering which 
+    // table we need to remove that data from and then make a database request to 
+    // remove the data. Once the data has been removed, we make a function request
+    // for the respective entity and display the data to the user so that they know
+    // that it has been removed.
     const removeEntity = async(entity, entity_id) => {
         if (entity && entity_id) {
             const removeData = await databaseQueries.deleteDatabaseEntry(entity, entity_id)
@@ -428,6 +497,12 @@ const userPrompts = () => {
             removeEntityPrompts();
         }
     };
+
+    // The removeUser, removeDepartment, and removeRole functions all work very similarly
+    // the purpose of these functions is to collect an "entity" and an "entity_id" that the
+    // user wants to remove. Entity tells us which data table we need to look at and entity ID
+    // tells us the ID of the entity that needs to be removed. Once we have collected this information
+    // it gets pushed to the removeEntity function to actually remove that entity from the database.
 
     const removeUser = async() => {
         const removeData = await databaseQueries.fetchAllEmployees()
@@ -513,6 +588,10 @@ const userPrompts = () => {
         });
     };
 
+    // We use this function to gather data about which type of entity the user
+    // wants to delete, for example, do they want to delete a department, a role, 
+    // or an employee. Once we know what the user wants to delete, we pass that information
+    // to a different function to actually delete the entity. 
     const removeEntityPrompts = async() => {
         inquirer
             .prompt(
@@ -547,7 +626,12 @@ const userPrompts = () => {
             });
     };
 
+    // Department ID is passed to this function and we use this to request
+    // a data table that has the data that we will be cycling through. 
     const fetchBudgetByDepartment = async(department_id) => {
+        // We initialize some variables that we will be using to hold data 
+        // that will be used at a later point. Most importantly here is the
+        // totalBudget variable that will hold the budget for the department.
         let usersByDepartment = {};
         let departmentName;
         let totalBudget = 0;
@@ -558,6 +642,10 @@ const userPrompts = () => {
             //console.log(usersByDepartment);
         })
         .then(() => {
+                // We iterate through the data we got back using a for each loop, 
+                // the reason for this is so that we can look at the salary of each entry
+                // we got back and add it to our totalBudget variable. Once we have iterated
+                // through all of the data, we should have a total budget for the department.
                 usersByDepartment.forEach((user) => {
                     //console.log(user.salary);
                     totalBudget += parseFloat(user.salary);
@@ -565,6 +653,7 @@ const userPrompts = () => {
             });
         })
         .then(() => {
+            // We console log the budget to the user here. 
             console.log(`Total Budget for Department "${departmentName}/${department_id}" is: ${totalBudget}.`);
             navMenu();
         })
@@ -573,6 +662,9 @@ const userPrompts = () => {
         });
     };
 
+    // We use this function to collect the department ID for which the user wants to
+    // view the budget of. Once we have this department ID, we pass it to our fetchBudgetByDepartment
+    // function to retrieve a data table that has the data we need. 
     const fetchBudgetByDepartmentPrompts = async() => {
         const budgetData = await databaseQueries.fetchAllDepartments()
         .then((data) => {
@@ -598,6 +690,12 @@ const userPrompts = () => {
         })
     };
 
+    // The "navMenu" function serves as our main navigation function. 
+    // When the app is started, we will launch this function will will
+    // present the user with a bunch of options that allows them to interact
+    // with out database. Depending on what the user selects we will take them
+    // into a sub menu or fetch some data from our database. We use inquirer
+    // prompt and switch cases to handle this part of our logic. 
     const navMenu = async () => {
         inquirer
             .prompt(
@@ -625,6 +723,12 @@ const userPrompts = () => {
                 ]
             ).then((userSelection) => {
                 console.log(userSelection);
+                // Switch cases  kick in here, depending on what the user has selected
+                // we will call a function that will either deliver data to the user
+                // or prompt them for some more questions before we deliver the data. 
+
+                // We take the users selection and we then use this selection to see 
+                // what the user wants to do. 
                 switch (userSelection.choice) {
                     case "View All Departments": fetchDepartments();
                         break;
@@ -655,8 +759,9 @@ const userPrompts = () => {
                 }
             });
     };
+    // navMenu function is called here to display the prompts to the end user. 
     navMenu();
 };
 
-
+// We initialize the app by calling our start function. 
 userPrompts();
