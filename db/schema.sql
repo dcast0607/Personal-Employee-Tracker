02a08751b013fab7811cdee2568a1d644f0784c8
dcast0607 <100454484+dcast0607@users.noreply.github.com>
2022-09-10 01:15:19 -0700

    Commenting on the schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,37 @@
+/* 
+We drop the database as part of our seeded data. 
+*/
 DROP DATABASE IF EXISTS employeesDB;
 
+/*
+Once the datbase has been droped we re-create it for a fresh database. 
+*/
 CREATE DATABASE employeesDB;
 
+/*
+We ask to use the "employeesDB" in our requests.
+*/
 USE employeesDB;
 
 
-
+/*
+We create a new table with the following definitions. 
+    - column 1 => id with requiring a value and auto-incrementing
+    - column 2 => name with a maxsize of 30 characters and requiring a value. 
+*/
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
 );
 
+/*
+We create a new employee table with the following definitions: 
+    - id
+    - first_name
+    - last_name
+    - role_id
+    - manager_id
+*/
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -19,6 +40,14 @@ CREATE TABLE employee (
     manager_id INT
 );
 
+
+/*
+We create a role table with the following information: 
+    - id
+    - title
+    - salary
+    - department_id
+*/
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
